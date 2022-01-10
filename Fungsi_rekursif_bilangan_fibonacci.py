876f29fd97c7e2974f369bfae3bd745bf9ece4d5
nahdluddin <93822283+NAHDLUDDIN@users.noreply.github.com>
2022-01-10 10:09:07 +0700

    Add files via upload

---
+++ Fungsi_rekursif_bilangan_fibonacci.py
@@ -0,0 +1,15 @@
+def fibonacci(n):
+    if n<0:
+        return 0
+    else:
+        if n < 2:
+            return n 
+        else:
+            return fibonacci(n-1) + fibonacci(n-2)
+x=int(input("masukkan angka: "))
+
+for i in range (x+1):
+    print("bilangan fibonacci ke: ",(i)," adalah = ",fibonacci(i))
+    
+
+
