4b676b34a191ab3f0dd5217a0ae52e0f3df9c5fb
nahdluddin <93822283+NAHDLUDDIN@users.noreply.github.com>
2022-01-10 12:30:13 +0700

    Add files via upload

---
+++ fibonacci_menggunakan_perulangan_for.py
@@ -0,0 +1,14 @@
+deret1=0
+deret2=1
+jumlah_deret =int (input("masukkan angka deret : "))
+for i in range (jumlah_deret+1):
+    print("bilangan fibonacci ke: ",(i)," adalah = ",deret1)
+    fibonacci=deret1+deret2
+    deret1=deret2
+    deret2=fibonacci
+
+
+    
+    
+
+
